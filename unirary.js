d0323f725d594965ec0ea07dd1b621411eb55541
Nimrajarral <nimrajarral@gmail.com>
2024-03-22 00:37:38 +0500

    first commit

---
+++ unirary.js
@@ -0,0 +1,25 @@
+var num = 5;
+++num;
+num--;
+--num;
+++num;
+--num;
+--num;
+--num;
+console.log(num);
+// postfix increment operator 
+var num1 = 5;
+num1++;
+console.log(num1);
+// prefix incremect operator
+var num2 = 7;
+++num2;
+console.log(num2);
+// postfix decrement operator 
+var num3 = 5;
+num3--;
+console.log(num3);
+// prefix decrement operator 
+var num4 = 7;
+--num4;
+console.log(num4);
---
+++ unirary.ts
@@ -0,0 +1,32 @@
+
+    //combination of postfix and prefix operator
+let num =5;
+++num;
+num--;
+--num;
+++num;
+--num;
+--num;
+--num;
+console.log(num)
+
+// postfix increment operator 
+let num1=5;
+num1++;
+console.log(num1);
+
+// prefix incremect operator
+let num2=7;
+++num2;
+console.log(num2);
+
+// postfix decrement operator 
+let num3=5;
+num3--;
+console.log(num3)
+    
+// prefix decrement operator 
+let num4=7;
+--num4;
+console.log(num4)
+ 
